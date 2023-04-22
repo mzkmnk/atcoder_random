2ebc84cd668ceb68217d926063a4bbdfc4b938c9
mizuki <sma0729mex@gmail.com>
2023-04-23 07:47:44 +0900

    update:コメント追加

--- atcoder_web.py
+++ atcoder_web.py
@@ -1,6 +1,17 @@
+"""
+これはatcoderの過去問であるABCの問題をランダムに選んでブラウザで開くスクリプトです。
+18行目にある['A', 'B', 'C', 'D','E']のリストを変更することで、選択する難易度を変更できます。
+コードを自由に改変したり、改変したコードを公開しても構いません。
+最近DDoSでUnratedになっていてモチベが下がっている人(自分も含め)や、問題を選ぶのが面倒な人は
+このスクリプトを使ってみてください！！
+またEx問題をリストに入れても表示されない？ので修正できたらします、、、笑
+"""
+
+
 import requests
 import random
 import webbrowser
+import time
 
 def get_atcoder_problems():
     url = "https://kenkoooo.com/atcoder/resources/problems.json"
@@ -9,7 +20,7 @@ def get_atcoder_problems():
 
 def select_random_problems(problems, count_per_difficulty):
     selected_problems = {}
-    for difficulty in ['A', 'B', 'C', 'D','E']:
+    for difficulty in ['A', 'B', 'C', 'D','E','F','G','Ex']:
         filtered_problems = [
             problem for problem in problems
             if problem['contest_id'].startswith('abc') and problem['id'].split('_')[-1].startswith(difficulty.lower())
@@ -29,6 +40,7 @@ def main():
             problem_url = f"https://atcoder.jp/contests/{problem_set[0]['contest_id']}/tasks/{problem_set[0]['id']}"
             print(f"{difficulty}問題: {problem_url}")
             webbrowser.open(problem_url)
+            time.sleep(2)
 
 if __name__ == "__main__":
     main()
